543199931f2ca1d249018c3c07ea73b3e86988b7
Carsten Uphoff <carsten.uphoff@intel.com>
2025-02-28 09:49:07 -0800

    Refactor xmx test
    Signed-off-by: Carsten Uphoff <carsten.uphoff@intel.com>

--- test/xmx.hpp
+++ test/xmx.hpp
@@ -16,9 +16,76 @@
 using runtime_class = RUNTIME_CLASS;
 using namespace tinytc;
 
-TEST_CASE(RUNTIME_NAME " store block2d f16") {
+template <typename T> class test_matrix {
+  public:
+    using value_type = T;
+    constexpr static T poison = -1293.f;
+
+    test_matrix(std::int64_t rows, std::int64_t cols, T initial_value = poison)
+        : rows_{rows}, cols_{cols}, data_(rows * cols, initial_value) {}
+
+    inline auto rows() const -> std::int64_t { return rows_; }
+    inline auto cols() const -> std::int64_t { return cols_; }
+    inline auto bytes() const -> std::size_t { return rows_ * cols_ * sizeof(T); }
+
+    inline auto data() -> T * { return data_.data(); }
+    inline auto data() const -> T const * { return data_.data(); }
+
+    inline auto operator()(std::int64_t i, std::int64_t j) -> T & { return data_[i + j * rows_]; }
+    inline auto operator()(std::int64_t i, std::int64_t j) const -> T const & {
+        return data_[i + j * rows_];
+    }
+
+  private:
+    std::int64_t rows_, cols_;
+    std::vector<T> data_;
+};
+
+template <typename... T>
+void run_custom_test_case(std::string const &code, char const *kernel_name, T &...matrices) {
+    struct buffer_pair {
+        void *host;
+        std::size_t size;
+        bool is_const;
+        runtime_class::mem_t device;
+    };
+
     auto gpu_rt = std::make_shared<runtime_class>();
 
+    auto buffers = std::vector<buffer_pair>{};
+    buffers.reserve(sizeof...(T));
+
+    (buffers.emplace_back(buffer_pair{const_cast<typename T::value_type *>(matrices.data()),
+                                      matrices.bytes(), std::is_const_v<T>, nullptr}),
+     ...);
+
+    for (auto &bp : buffers) {
+        bp.device = gpu_rt->create_buffer(bp.size);
+        gpu_rt->memcpy_h2d(bp.device, bp.host, bp.size);
+    }
+
+    auto ctx = make_compiler_context();
+    ctx.set_error_reporter(
+        [](char const *what, const tinytc_location_t *, void *) { std::cerr << what << std::endl; },
+        nullptr);
+    auto bundle = gpu_rt->get_kernel_bundle(parse_string(code, ctx));
+    auto kernel = gpu_rt->get_kernel(bundle, kernel_name);
+
+    for (std::size_t i = 0; i < buffers.size(); ++i) {
+        gpu_rt->set_mem_arg(kernel, i, buffers[i].device, auto_mem_type_v<runtime_class::mem_t>);
+    }
+    gpu_rt->submit(kernel);
+    gpu_rt->synchronize();
+
+    for (auto &bp : buffers) {
+        if (!bp.is_const) {
+            gpu_rt->memcpy_d2h(bp.host, bp.device, bp.size);
+        }
+        gpu_rt->free_buffer(bp.device);
+    }
+}
+
+TEST_CASE(RUNTIME_NAME " store block2d f16") {
     const std::string code = R"TinyTL(
 func @store_block2d(%A: memref<f16x128x128> {alignment=128})
     attributes{subgroup_size=16,work_group_size=[16,1]} {
@@ -34,43 +101,26 @@ func @store_block2d(%A: memref<f16x128x128> {alignment=128})
     }
 })TinyTL";
 
-    constexpr auto poison = half(-1293.0f);
-    auto A_host = std::vector<half>(128 * 128, poison);
+    constexpr std::int64_t N = 128;
 
-    auto A = gpu_rt->create_buffer(128 * 128 * sizeof(half));
-    gpu_rt->memcpy_h2d(A, A_host.data(), A_host.size() * sizeof(half));
+    auto A = test_matrix<half>(N, N);
 
-    auto ctx = make_compiler_context();
-    ctx.set_error_reporter(
-        [](char const *what, const tinytc_location_t *, void *) { std::cerr << what << std::endl; },
-        nullptr);
-    auto bundle = gpu_rt->get_kernel_bundle(parse_string(code, ctx));
-    auto kernel = gpu_rt->get_kernel(bundle, "store_block2d");
+    run_custom_test_case(code, "store_block2d", A);
 
-    gpu_rt->set_mem_arg(kernel, 0, A, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->submit(kernel);
-    gpu_rt->synchronize();
-
-    gpu_rt->memcpy_d2h(A_host.data(), A, A_host.size() * sizeof(half));
-
-    for (std::size_t j = 0; j < 128; ++j) {
-        for (std::size_t i = 0; i < 128; ++i) {
+    for (std::int64_t j = 0; j < A.cols(); ++j) {
+        for (std::int64_t i = 0; i < A.rows(); ++i) {
             if (i >= 32 && i < 48 && j >= 64 && j < 72) {
-                REQUIRE(A_host[i + j * 128] == half(42.0f));
+                REQUIRE(A(i, j) == half(42.0f));
             } else if (i >= 62 && i < 94 && j >= 17 && j < 33) {
-                REQUIRE(A_host[i + j * 128] == half(43.0f));
+                REQUIRE(A(i, j) == half(43.0f));
             } else {
-                REQUIRE(A_host[i + j * 128] == poison);
+                REQUIRE(A(i, j) == A.poison);
             }
         }
     }
-
-    gpu_rt->free_buffer(A);
 }
 
 TEST_CASE(RUNTIME_NAME " load block2d f16") {
-    auto gpu_rt = std::make_shared<runtime_class>();
-
     const std::string code = R"TinyTL(
 func @load_block2d(%A: memref<f16x128x128> {alignment=128},
                    %B: memref<f16x128x128> {alignment=128})
@@ -83,42 +133,26 @@ func @load_block2d(%A: memref<f16x128x128> {alignment=128},
     }
 })TinyTL";
 
-    constexpr auto poison = half(-1293.0f);
-    auto A_host = std::vector<half>(128 * 128, poison);
-    auto B_host = std::vector<half>(128 * 128, poison);
-    for (std::size_t j = 8; j < 24; ++j) {
-        for (std::size_t i = 4; i < 36; ++i) {
-            A_host[i + j * 128] = half(static_cast<float>(i + j * 128));
+    constexpr std::int64_t N = 128;
+
+    const auto A = [] {
+        auto A = test_matrix<half>(N, N);
+        for (std::int64_t j = 8; j < 24; ++j) {
+            for (std::int64_t i = 4; i < 36; ++i) {
+                A(i, j) = half(static_cast<float>(i + j * A.rows()));
+            }
+        }
+        return A;
+    }();
+    auto B = test_matrix<half>(N, N);
+
+    run_custom_test_case(code, "load_block2d", A, B);
+
+    for (std::int64_t j = 0; j < A.cols(); ++j) {
+        for (std::int64_t i = 0; i < A.rows(); ++i) {
+            REQUIRE(A(i, j) == B(i, j));
         }
     }
-
-    auto A = gpu_rt->create_buffer(128 * 128 * sizeof(half));
-    auto B = gpu_rt->create_buffer(128 * 128 * sizeof(half));
-    gpu_rt->memcpy_h2d(A, A_host.data(), A_host.size() * sizeof(half));
-    gpu_rt->memcpy_h2d(B, B_host.data(), B_host.size() * sizeof(half));
-
-    auto ctx = make_compiler_context();
-    ctx.set_error_reporter(
-        [](char const *what, const tinytc_location_t *, void *) { std::cerr << what << std::endl; },
-        nullptr);
-    auto bundle = gpu_rt->get_kernel_bundle(parse_string(code, ctx));
-    auto kernel = gpu_rt->get_kernel(bundle, "load_block2d");
-
-    gpu_rt->set_mem_arg(kernel, 0, A, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->set_mem_arg(kernel, 1, B, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->submit(kernel);
-    gpu_rt->synchronize();
-
-    gpu_rt->memcpy_d2h(B_host.data(), B, B_host.size() * sizeof(half));
-
-    for (std::size_t j = 0; j < 128; ++j) {
-        for (std::size_t i = 0; i < 128; ++i) {
-            REQUIRE(A_host[i + j * 128] == B_host[i + j * 128]);
-        }
-    }
-
-    gpu_rt->free_buffer(A);
-    gpu_rt->free_buffer(B);
 }
 
 TEST_CASE(RUNTIME_NAME " matmul dpas f16") {
@@ -139,52 +173,39 @@ func @matmul_dpas(%A: memref<f16x64x64> {alignment=64},
     }
 })TinyTL";
 
-    constexpr auto poison = half(-1293.0f);
-    auto A_host = std::vector<half>(64 * 64, poison);
-    auto B_host = std::vector<half>(64 * 64, poison);
-    auto C_host = std::vector<float>(64 * 64, poison);
+    constexpr std::int64_t N = 64;
+
     constexpr double f16_eps = 0.0009765625;
-    for (std::size_t j = 0; j < 64; ++j) {
-        for (std::size_t i = 0; i < 64; ++i) {
-            A_host[i + j * 64] = half(static_cast<float>((1.0 + i * f16_eps) * (j + 1) / 32));
-            B_host[i + j * 64] = half(static_cast<float>(1.0 / ((i + 1) * (1 + j * f16_eps))));
+    const auto A = [] {
+        auto A = test_matrix<half>(N, N);
+        for (std::int64_t j = 0; j < N; ++j) {
+            for (std::int64_t i = 0; i < N; ++i) {
+                A(i, j) = half(static_cast<float>((1.0 + i * f16_eps) * (j + 1) / 32));
+            }
         }
-    }
+        return A;
+    }();
+    const auto B = [] {
+        auto B = test_matrix<half>(N, N);
+        for (std::int64_t j = 0; j < N; ++j) {
+            for (std::int64_t i = 0; i < N; ++i) {
+                B(i, j) = half(static_cast<float>(1.0 / ((i + 1) * (1 + j * f16_eps))));
+            }
+        }
+        return B;
+    }();
+    auto C = test_matrix<float>(N, N);
 
-    auto A = gpu_rt->create_buffer(64 * 64 * sizeof(half));
-    auto B = gpu_rt->create_buffer(64 * 64 * sizeof(half));
-    auto C = gpu_rt->create_buffer(64 * 64 * sizeof(float));
-    gpu_rt->memcpy_h2d(A, A_host.data(), A_host.size() * sizeof(half));
-    gpu_rt->memcpy_h2d(B, B_host.data(), B_host.size() * sizeof(half));
-    gpu_rt->memcpy_h2d(C, C_host.data(), C_host.size() * sizeof(float));
-
-    auto ctx = make_compiler_context();
-    ctx.set_error_reporter(
-        [](char const *what, const tinytc_location_t *, void *) { std::cerr << what << std::endl; },
-        nullptr);
-    auto bundle = gpu_rt->get_kernel_bundle(parse_string(code, ctx));
-    auto kernel = gpu_rt->get_kernel(bundle, "matmul_dpas");
-
-    gpu_rt->set_mem_arg(kernel, 0, A, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->set_mem_arg(kernel, 1, B, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->set_mem_arg(kernel, 2, C, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->submit(kernel);
-    gpu_rt->synchronize();
-
-    gpu_rt->memcpy_d2h(C_host.data(), C, C_host.size() * sizeof(float));
+    run_custom_test_case(code, "matmul_dpas", A, B, C);
 
     constexpr auto tol = 0.00195790082582908423;
-    for (std::size_t j = 0; j < 16; ++j) {
-        for (std::size_t i = 0; i < 32; ++i) {
-            REQUIRE(C_host[i + j * 64] ==
+    for (std::int64_t j = 0; j < 16; ++j) {
+        for (std::int64_t i = 0; i < 32; ++i) {
+            REQUIRE(C(i, j) ==
                     doctest::Approx(static_cast<float>(1 + i * f16_eps) / (1 + j * f16_eps))
                         .epsilon(tol));
         }
     }
-
-    gpu_rt->free_buffer(A);
-    gpu_rt->free_buffer(B);
-    gpu_rt->free_buffer(C);
 }
 
 TEST_CASE(RUNTIME_NAME " matmul B transposed dpas f16") {
@@ -192,63 +213,46 @@ TEST_CASE(RUNTIME_NAME " matmul B transposed dpas f16") {
 
     const std::string code = R"TinyTL(
 func @matmul_dpas(%A: memref<f16x64x64>,
-                  %B: memref<f16x64x64>,
+                  %B: memref<f16x32x64>,
                   %C: memref<f32x64x64>)
     attributes{subgroup_size=16,work_group_size=[16,1]} {
     parallel {
         %0 = constant 0 : index
-        %1 = cooperative_matrix_load.n %A[%0,%0] : coopmatrix<f16x16x16,matrix_a>
-        %2 = cooperative_matrix_load.t %B[%0,%0] : coopmatrix<f16x16x8,matrix_b>
-        %3 = constant 0.0 : coopmatrix<f32x16x8,matrix_acc>
-        %4 = cooperative_matrix_mul_add %1, %2, %3 : coopmatrix<f32x16x8,matrix_acc>
+        %1 = cooperative_matrix_load.n %A[%0,%0] : coopmatrix<f16x32x32,matrix_a>
+        %2 = cooperative_matrix_load.t %B[%0,%0] : coopmatrix<f16x32x32,matrix_b>
+        %3 = constant 0.0 : coopmatrix<f32x32x32,matrix_acc>
+        %4 = cooperative_matrix_mul_add %1, %2, %3 : coopmatrix<f32x32x32,matrix_acc>
         cooperative_matrix_store %4, %C[%0,%0]
     }
 })TinyTL";
 
-    constexpr auto poison = half(-1293.0f);
-    auto A_host = std::vector<half>(64 * 64, half(0.0f));
-    auto B_host = std::vector<half>(64 * 64, half(0.0f));
-    auto C_host = std::vector<float>(64 * 64, poison);
-    for (std::size_t i = 0; i < 64; ++i) {
-        A_host[i + i * 64] = half(1.0f);
-    }
-    for (std::size_t j = 0; j < 64; ++j) {
-        for (std::size_t i = 0; i < 64; ++i) {
-            B_host[i + j * 64] = half(static_cast<float>(i + j * 64));
+    constexpr std::int64_t M = 64, N = 32, K = 64;
+
+    const auto A = [] {
+        auto A = test_matrix<half>(M, K, 0.0f);
+        for (std::int64_t i = 0; i < M; ++i) {
+            A(i, i) = half(1.0f);
+        }
+        return A;
+    }();
+    const auto B = [] {
+        auto B = test_matrix<half>(N, K);
+        for (std::int64_t j = 0; j < K; ++j) {
+            for (std::int64_t i = 0; i < N; ++i) {
+                B(i, j) = half(static_cast<float>(i + j * N));
+            }
+        }
+        return B;
+    }();
+    auto C = test_matrix<float>(M, N);
+
+    run_custom_test_case(code, "matmul_dpas", A, B, C);
+
+    for (std::int64_t j = 0; j < 32; ++j) {
+        for (std::int64_t i = 0; i < 32; ++i) {
+            REQUIRE(C(i, j) == B(j, i));
         }
     }
-
-    auto A = gpu_rt->create_buffer(A_host.size() * sizeof(half));
-    auto B = gpu_rt->create_buffer(B_host.size() * sizeof(half));
-    auto C = gpu_rt->create_buffer(C_host.size() * sizeof(float));
-    gpu_rt->memcpy_h2d(A, A_host.data(), A_host.size() * sizeof(half));
-    gpu_rt->memcpy_h2d(B, B_host.data(), B_host.size() * sizeof(half));
-    gpu_rt->memcpy_h2d(C, C_host.data(), C_host.size() * sizeof(float));
-
-    auto ctx = make_compiler_context();
-    ctx.set_error_reporter(
-        [](char const *what, const tinytc_location_t *, void *) { std::cerr << what << std::endl; },
-        nullptr);
-    auto bundle = gpu_rt->get_kernel_bundle(parse_string(code, ctx));
-    auto kernel = gpu_rt->get_kernel(bundle, "matmul_dpas");
-
-    gpu_rt->set_mem_arg(kernel, 0, A, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->set_mem_arg(kernel, 1, B, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->set_mem_arg(kernel, 2, C, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->submit(kernel);
-    gpu_rt->synchronize();
-
-    gpu_rt->memcpy_d2h(C_host.data(), C, C_host.size() * sizeof(float));
-
-    for (std::size_t j = 0; j < 8; ++j) {
-        for (std::size_t i = 0; i < 16; ++i) {
-            REQUIRE(C_host[i + j * 64] == B_host[j + i * 64]);
-        }
-    }
-
-    gpu_rt->free_buffer(A);
-    gpu_rt->free_buffer(B);
-    gpu_rt->free_buffer(C);
 }
 
 TEST_CASE(RUNTIME_NAME " load and store block2d on local memory f16") {
@@ -279,47 +283,31 @@ func @load_store_block2d_slm(%A: memref<f16x128x128> {alignment=128},
     }
 })TinyTL";
 
-    constexpr auto poison = half(-1293.0f);
-    auto A_host = std::vector<half>(128 * 128, poison);
-    auto B_host = std::vector<half>(128 * 128, poison);
-    for (std::size_t j = 8; j < 16; ++j) {
-        for (std::size_t i = 4; i < 20; ++i) {
-            A_host[i + j * 128] = half(static_cast<float>(i + j * 128));
+    constexpr std::int64_t N = 128;
+
+    const auto A = [] {
+        auto A = test_matrix<half>(N, N);
+        for (std::int64_t j = 8; j < 16; ++j) {
+            for (std::int64_t i = 4; i < 20; ++i) {
+                A(i, j) = half(static_cast<float>(i + j * N));
+            }
+        }
+        for (std::int64_t j = 32; j < 64; ++j) {
+            for (std::int64_t i = 64; i < 96; ++i) {
+                A(i, j) = half(static_cast<float>(i + j * N));
+            }
+        }
+        return A;
+    }();
+    auto B = test_matrix<half>(N, N);
+
+    run_custom_test_case(code, "load_store_block2d_slm", A, B);
+
+    for (std::int64_t j = 0; j < N; ++j) {
+        for (std::int64_t i = 0; i < N; ++i) {
+            REQUIRE(A(i, j) == B(i, j));
         }
     }
-    for (std::size_t j = 32; j < 64; ++j) {
-        for (std::size_t i = 64; i < 96; ++i) {
-            A_host[i + j * 128] = half(static_cast<float>(i + j * 128));
-        }
-    }
-
-    auto A = gpu_rt->create_buffer(128 * 128 * sizeof(half));
-    auto B = gpu_rt->create_buffer(128 * 128 * sizeof(half));
-    gpu_rt->memcpy_h2d(A, A_host.data(), A_host.size() * sizeof(half));
-    gpu_rt->memcpy_h2d(B, B_host.data(), B_host.size() * sizeof(half));
-
-    auto ctx = make_compiler_context();
-    ctx.set_error_reporter(
-        [](char const *what, const tinytc_location_t *, void *) { std::cerr << what << std::endl; },
-        nullptr);
-    auto bundle = gpu_rt->get_kernel_bundle(parse_string(code, ctx));
-    auto kernel = gpu_rt->get_kernel(bundle, "load_store_block2d_slm");
-
-    gpu_rt->set_mem_arg(kernel, 0, A, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->set_mem_arg(kernel, 1, B, auto_mem_type_v<runtime_class::mem_t>);
-    gpu_rt->submit(kernel);
-    gpu_rt->synchronize();
-
-    gpu_rt->memcpy_d2h(B_host.data(), B, B_host.size() * sizeof(half));
-
-    for (std::size_t j = 0; j < 128; ++j) {
-        for (std::size_t i = 0; i < 128; ++i) {
-            REQUIRE(A_host[i + j * 128] == B_host[i + j * 128]);
-        }
-    }
-
-    gpu_rt->free_buffer(A);
-    gpu_rt->free_buffer(B);
 }
 
 #endif // XMX_20250120_HPP
