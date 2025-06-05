d56f670154ad3cf50e1f69e25c2dcf3dac0a7604
Carsten Uphoff <carsten.uphoff@intel.com>
2025-06-05 10:18:08 +0000

    Add optimization to merge multiple cols in single block load/write
    Signed-off-by: Carsten Uphoff <carsten.uphoff@intel.com>

--- src/spv/coopmatrix_impl_block.cpp
+++ src/spv/coopmatrix_impl_block.cpp
@@ -29,7 +29,9 @@
 
 namespace tinytc::spv {
 
-constexpr std::int64_t max_block_io_vec_size = 8;
+auto max_block_io_vec_size(scalar_type sty) -> std::int64_t {
+    return sty == scalar_type::i8 || sty == scalar_type::i16 ? 16 : 8;
+}
 
 auto coopmatrix_impl_block::load(cooperative_matrix_load_inst const &in, dope_vector const &odv,
                                  spv_inst *operand, spv_inst *pos0, spv_inst *pos1) -> spv_inst * {
@@ -50,23 +52,40 @@ auto coopmatrix_impl_block::load(cooperative_matrix_load_inst const &in, dope_ve
         return coopmatrix_impl::load(in, odv, operand, pos0, pos1);
     }
 
+    auto walker = matrix_walker(unique(), cfg().subgroup_size, layout, pos0, pos1, odv.shape(0),
+                                odv.shape(1), odv.stride(0), odv.stride(1), in.checked(), 0);
+
+    const auto io_sty = get_io_sty(sty);
+    const std::int32_t blocks_per_load =
+        is_positive_power_of_two(layout.blocks)
+            ? std::min(layout.blocks, max_block_io_vec_size(io_sty))
+            : 1;
+    const std::int32_t cols_per_load = [&]() -> std::int32_t {
+        const auto ot = get_memref_type(in.operand());
+        const bool is_contiguous = ot->dim() == 2 && ot->shape(0) == rt->shape(0) &&
+                                   ot->stride(0) == 1 && ot->stride(1) == ot->shape(0);
+        std::int32_t cols_per_load = 1;
+        if (is_contiguous && !walker.cols_checked()) {
+            const std::int32_t max_cols_per_load = max_block_io_vec_size(io_sty) / blocks_per_load;
+            const std::int32_t num_cols = layout.length / layout.blocks;
+            while (2 * cols_per_load <= max_cols_per_load && num_cols % (2 * cols_per_load) == 0) {
+                cols_per_load *= 2;
+            }
+        }
+        return cols_per_load;
+    }();
+
     const auto matrix_ty = spv_ty(layout);
     const auto interface_ty = spv_interface_ty(layout);
-    const auto io_sty = get_io_sty(sty);
-    const std::int32_t blocks_per_load = is_positive_power_of_two(layout.blocks)
-                                             ? std::min(layout.blocks, max_block_io_vec_size)
-                                             : 1;
     auto io_ty = unique().scalar_ty(io_sty);
-    spv_inst *io_vec_ty = blocks_per_load > 1 ? unique().vec_ty(io_ty, blocks_per_load) : io_ty;
+    const auto io_vec_size = blocks_per_load * cols_per_load;
+    spv_inst *io_vec_ty = io_vec_size ? unique().vec_ty(io_ty, io_vec_size) : io_ty;
     const auto pointer_ty = [&] {
         auto ot = get_memref_type(in.operand());
         const auto storage_cls = address_space_to_storage_class(ot->addrspace());
         const auto align = ot->element_alignment();
         return unique().pointer_ty(storage_cls, io_ty, align);
     }();
-
-    auto walker = matrix_walker(unique(), cfg().subgroup_size, layout, pos0, pos1, odv.shape(0),
-                                odv.shape(1), odv.stride(0), odv.stride(1), in.checked(), 0);
 
     auto &mod = unique().mod();
     operand = mod.add<OpBitcast>(pointer_ty, operand);
@@ -84,16 +103,18 @@ auto coopmatrix_impl_block::load(cooperative_matrix_load_inst const &in, dope_ve
     };
     auto const ld_block = [&](tinytc_spv_mod &mod) {
         spv_inst *block_result = result;
-        for (std::int64_t u = 0; u < layout.length / layout.blocks; ++u) {
+        for (std::int64_t u = 0; u < layout.length / layout.blocks; u += cols_per_load) {
             spv_inst *val = walker.needs_mask() || walker.cols_checked() ? ld_chk(mod) : ld(mod);
             if (blocks_per_load > 1) {
-                for (std::int32_t b = 0; b < blocks_per_load; ++b) {
-                    spv_inst *v =
-                        mod.add<OpCompositeExtract>(io_ty, val, std::vector<LiteralInteger>{b});
-                    v = mod.add<OpBitcast>(interface_ty, v);
-                    const auto comp_no =
-                        layout.component_no(walker.col_no(), walker.block_no() + b);
-                    block_result = insert(layout, v, block_result, comp_no);
+                for (std::int32_t c = 0; c < cols_per_load; ++c) {
+                    for (std::int32_t b = 0; b < blocks_per_load; ++b) {
+                        spv_inst *v = mod.add<OpCompositeExtract>(
+                            io_ty, val, std::vector<LiteralInteger>{b + c * blocks_per_load});
+                        v = mod.add<OpBitcast>(interface_ty, v);
+                        const auto comp_no =
+                            layout.component_no(walker.col_no() + c, walker.block_no() + b);
+                        block_result = insert(layout, v, block_result, comp_no);
+                    }
                 }
             } else {
                 val = mod.add<OpBitcast>(interface_ty, val);
@@ -101,7 +122,9 @@ auto coopmatrix_impl_block::load(cooperative_matrix_load_inst const &in, dope_ve
             }
 
             if (u < layout.cols - 1) {
-                walker.advance_column();
+                for (std::int32_t c = 0; c < cols_per_load; ++c) {
+                    walker.advance_column();
+                }
             }
         }
         return block_result;
@@ -139,12 +162,34 @@ void coopmatrix_impl_block::store(cooperative_matrix_store_inst const &in, dope_
         return;
     }
 
+    auto walker = matrix_walker(unique(), cfg().subgroup_size, layout, pos0, pos1, odv.shape(0),
+                                odv.shape(1), odv.stride(0), odv.stride(1), in.checked(), 0);
+
     const auto io_sty = get_io_sty(sty);
-    const std::int32_t blocks_per_store = is_positive_power_of_two(layout.blocks)
-                                              ? std::min(layout.blocks, max_block_io_vec_size)
-                                              : 1;
+    const std::int32_t blocks_per_store =
+        is_positive_power_of_two(layout.blocks)
+            ? std::min(layout.blocks, max_block_io_vec_size(io_sty))
+            : 1;
+    const std::int32_t cols_per_store = [&]() -> std::int32_t {
+        const auto ot = get_memref_type(in.operand());
+        const bool is_contiguous = ot->dim() == 2 && ot->shape(0) == vt->shape(0) &&
+                                   ot->stride(0) == 1 && ot->stride(1) == ot->shape(0);
+        std::int32_t cols_per_store = 1;
+        if (is_contiguous && !walker.cols_checked()) {
+            const std::int32_t max_cols_per_store =
+                max_block_io_vec_size(io_sty) / blocks_per_store;
+            const std::int32_t num_cols = layout.length / layout.blocks;
+            while (2 * cols_per_store <= max_cols_per_store &&
+                   num_cols % (2 * cols_per_store) == 0) {
+                cols_per_store *= 2;
+            }
+        }
+        return cols_per_store;
+    }();
+
     auto io_ty = unique().scalar_ty(io_sty);
-    spv_inst *io_vec_ty = blocks_per_store > 1 ? unique().vec_ty(io_ty, blocks_per_store) : io_ty;
+    auto const io_vec_size = blocks_per_store * cols_per_store;
+    spv_inst *io_vec_ty = io_vec_size > 1 ? unique().vec_ty(io_ty, io_vec_size) : io_ty;
     const auto pointer_ty = [&] {
         auto ot = get_memref_type(in.operand());
         const auto storage_cls = address_space_to_storage_class(ot->addrspace());
@@ -152,15 +197,12 @@ void coopmatrix_impl_block::store(cooperative_matrix_store_inst const &in, dope_
         return unique().pointer_ty(storage_cls, io_ty, align);
     }();
 
-    auto walker = matrix_walker(unique(), cfg().subgroup_size, layout, pos0, pos1, odv.shape(0),
-                                odv.shape(1), odv.stride(0), odv.stride(1), in.checked(), 0);
-
     auto &mod = unique().mod();
     operand = mod.add<OpBitcast>(pointer_ty, operand);
 
     for (std::int64_t w = 0; w < layout.blocks; w += blocks_per_store) {
         auto const st_block = [&](tinytc_spv_mod &mod) {
-            for (std::int64_t u = 0; u < layout.length / layout.blocks; ++u) {
+            for (std::int64_t u = 0; u < layout.length / layout.blocks; u += cols_per_store) {
                 const auto st = [&](tinytc_spv_mod &mod) {
                     spv_inst *offset = walker.offset();
                     auto pointer = mod.add<OpInBoundsPtrAccessChain>(pointer_ty, operand, offset,
@@ -168,13 +210,16 @@ void coopmatrix_impl_block::store(cooperative_matrix_store_inst const &in, dope_
                     spv_inst *val_ij = nullptr;
                     if (blocks_per_store > 1) {
                         val_ij = mod.add<OpUndef>(io_vec_ty);
-                        for (std::int32_t b = 0; b < blocks_per_store; ++b) {
-                            const auto comp_no =
-                                layout.component_no(walker.col_no(), walker.block_no() + b);
-                            spv_inst *v = extract(layout, val, comp_no);
-                            v = mod.add<OpBitcast>(io_ty, v);
-                            val_ij = mod.add<OpCompositeInsert>(io_vec_ty, v, val_ij,
-                                                                std::vector<LiteralInteger>{b});
+                        for (std::int32_t c = 0; c < cols_per_store; ++c) {
+                            for (std::int32_t b = 0; b < blocks_per_store; ++b) {
+                                const auto comp_no =
+                                    layout.component_no(walker.col_no() + c, walker.block_no() + b);
+                                spv_inst *v = extract(layout, val, comp_no);
+                                v = mod.add<OpBitcast>(io_ty, v);
+                                val_ij = mod.add<OpCompositeInsert>(
+                                    io_vec_ty, v, val_ij,
+                                    std::vector<LiteralInteger>{b + c * blocks_per_store});
+                            }
                         }
                     } else {
                         val_ij = extract(layout, val, walker.component_no());
@@ -189,7 +234,9 @@ void coopmatrix_impl_block::store(cooperative_matrix_store_inst const &in, dope_
                 }
 
                 if (u < layout.cols - 1) {
-                    walker.advance_column();
+                    for (std::int32_t c = 0; c < cols_per_store; ++c) {
+                        walker.advance_column();
+                    }
                 }
             }
         };
