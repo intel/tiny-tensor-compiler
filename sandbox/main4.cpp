6eb732ebe949910de766fd732f072d2284624449
tewart <timothee.ewart@intel.com>
2026-01-21 16:45:04 +0100

    wip

--- sandbox/main4.cpp
+++ sandbox/main4.cpp
@@ -59,49 +59,28 @@ int main(int argc, char *argv[]) {
 
 
     try {
+	// original size
+	const int64_t size = 64;
         // Initialize tensors
-        matrix<float> A(36, 32, 1);
-        matrix<float> B(36, 32, 0);
+        matrix<float> A(size, size, 1);
+        matrix<float> B(size, size, 0);
 
         const std::string code = R"TinyTL(
-func @tilling(%A: memref<f32x36x32> {alignment=128},
-              %B: memref<f32x36x32> {alignment=128})        
+func @tilling(%A: memref<f32x?x?,strided<1,?>> {alignment=128},
+              %B: memref<f32x?x?,strided<1,?>> {alignment=128})        
      attributes{subgroup_size=16,work_group_size=[16,16]} {
     ; alias
     $mat_t = coopmatrix<f32x16x16,matrix_acc>
+    ; creates tiles matrices of 16 x 16
     %c0 = constant 0 : index
-    %c1 = constant 36 : index
-    %c11 = constant 32 : index
-    %c2 = constant 2.0 : $mat_t 
-    %c4 = constant 3.0 : $mat_t 
+    %c1 = constant 1.0 : $mat_t 
+    %c16 = constant 16 : index 
 
- ; def: shape w = 16, h = 16, size = h * w
- ; i,j are the beginning of every tile
- ; I understand ti and tj are the reminders  
- ; c0 and c1, c11 must be the total size of the matrix 
- ; 16,16 I think is the shape of the tilling
- ; t_i = min(tile_shape_i , to_i - var_i) (reminder)
- ; foreach_tile (var_i, ...) = (from_i, ...),(to_i, ...)
-
-;  %toto = constant 4 : index
-; here I check the reminder
-    foreach_tile (%i,%j)=(%c0,%c0),(%c1,%c11) as (%ti,%tj)<=(16,16) {
-        %c3 = constant 16 : index
-; I just check the reminder on the number of row here 4 <16
-        %is_remainder = less_than %ti, %c3 : bool
-; variant with the exact value of the reminder
-;       %is_remainder = equal %ti, %toto : bool
-        if %is_remainder {
-            %tile = cooperative_matrix_load %A[%i,%j] : $mat_t
-            %tile_final = add %tile, %c4 : $mat_t
-; I suppose it know the dimension of b and the position of the Tile so it can check if there is "overwrite" beyond the last row
-            cooperative_matrix_store.rows_checked %tile_final, %B[%i,%j]
-        } else {
-; normal tile there is nothing to check
-             %tile = cooperative_matrix_load.rows_checked %A[%i,%j] : $mat_t
-             %tile_final = add %tile, %c2 : $mat_t
-             cooperative_matrix_store %tile_final, %B[%i,%j]
-        }
+    parallel {
+    %sv = subview %A[%c0:%c16,%c0:%c16] : memref<f32x?x?,strided<1,?>>
+    %tile = cooperative_matrix_load.both_checked %sv[%c0,%c0] : $mat_t
+    %tile_final = add %tile, %c1 : $mat_t
+    cooperative_matrix_store %tile_final, %B[%c0,%c0]
     }
 })TinyTL";
 
@@ -115,14 +94,17 @@ func @tilling(%A: memref<f32x36x32> {alignment=128},
         // kernel workgroup size can not be defined at runtime in tinytc right now this function do
         // the transpose between the block/block size of Carsten to the "Sycl" range
 	// 9 workgroup each work group has size 16 x 16 
-        const auto global_range = sycl::range<3u>{1,3,3};
+	const int64_t tile_size = 16;
+	const std::size_t gr_size = 1 + (A.rows() - 1) / tile_size; 
+        const auto global_range = sycl::range<3u>{1, gr_size, gr_size  };
+
+        std::cout << "gr_size: " << gr_size << std::endl;
 
         q.submit([&](sycl::handler &h) {
              h.set_args(A.data(),B.data());
              h.parallel_for(tinytc::get_global_size(global_range, sycl::range<3u>(1,16,16)), kernel);
          }).wait();
 
-        std::cout << A << std::endl;
         std::cout << B << std::endl;
     } catch (tinytc::status const &st) {
         std::cerr << "Error (" << static_cast<int>(st) << "): " << tinytc::to_string(st)
